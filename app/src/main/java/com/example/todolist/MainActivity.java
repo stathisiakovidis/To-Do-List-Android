f8269743e95ab6f50c2847d59d3882be9f45da54
grode <dimitris23bp@gmail.com>
2019-12-02 23:02:46 +0200

    Begin of the first two activities

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.todolist;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,11 +9,15 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String INFO = "Info";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +34,28 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
+    //Creates option in menu (in example settings)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+//        ArrayList<String> titles = new ArrayList<>();
+//        titles.add("Most Recently");
+//        titles.add("With date");
+//
+//        Spinner spinner = findViewById(R.id.spinner);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_spinner_item, titles);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//
+//        spinner.setAdapter(adapter);
+
         return true;
     }
 
@@ -45,9 +66,22 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id){
+            case R.id.action_settings:
+                Log.i(INFO, "Settings option is clicked");
+
+                break;
+            case R.id.action_calendar:
+                //change activity
+                Log.i(INFO, "Calendar option is clicked");
+                Intent myIntent = new Intent(this, CalendarActivity.class);
+                startActivity(myIntent);
+                break;
+            case R.id.action_indefinite:
+                Log.i(INFO, "Indefinite option is clicked");
+                break;
+            default:
+
         }
 
         return super.onOptionsItemSelected(item);
