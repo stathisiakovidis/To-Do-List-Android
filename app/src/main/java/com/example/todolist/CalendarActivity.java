45f841f2f227d89070fc3126a0a84f7d0aef5576
grode <dimitris23bp@gmail.com>
2019-12-10 21:53:09 +0200

    Renewed Calendar

--- app/src/main/java/com/example/todolist/CalendarActivity.java
+++ app/src/main/java/com/example/todolist/CalendarActivity.java
@@ -1,18 +1,42 @@
 package com.example.todolist;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class CalendarActivity extends Activity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+
+        toolbar.setNavigationOnClickListener((View v) -> {
+            Intent mainIntent = new Intent(this, MainActivity.class);
+            startActivity(mainIntent);
 
 
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
-
-
 
 }
