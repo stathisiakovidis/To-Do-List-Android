140620fe981f769a3e4ed2050ae0f4909a87c1c6
grode <dimitris23bp@gmail.com>
2019-12-10 20:05:00 +0200

    Higher version of API

--- app/src/main/java/com/example/todolist/CalendarActivity.java
+++ app/src/main/java/com/example/todolist/CalendarActivity.java
@@ -1,18 +1,14 @@
 package com.example.todolist;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class CalendarActivity extends Activity {
+public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-
-
     }
-
-
-
 }
