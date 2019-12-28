82f05d49d28e5f88681761b44f48d8b29c7ae038
grode <dimitris23bp@gmail.com>
2019-12-28 18:32:54 +0200

    Added onLongClickListener to date TextView

--- app/src/main/java/com/example/todolist/ItemActivity.java
+++ app/src/main/java/com/example/todolist/ItemActivity.java
@@ -46,21 +46,10 @@ public class ItemActivity extends AppCompatActivity {
         dateText = findViewById(R.id.dateText);
         body = findViewById(R.id.body);
 
-        dateText.setOnClickListener(v -> {
-
-            Calendar calendar = Calendar.getInstance();
-            int currYear = calendar.get(Calendar.YEAR);
-            int currMonth = calendar.get(Calendar.MONTH);
-            int currDay = calendar.get(Calendar.DAY_OF_MONTH);
-
-            DatePickerDialog datePickerDialog = new DatePickerDialog(ItemActivity.this,
-                    (datePicker, year, month, day) -> {
-                        dateText.setText(day + "\\" + month + "\\" + year );
-
-                    }, currYear, currMonth, currDay);
-            datePickerDialog.show();
-        });
-
+        //Add listeners to date textView
+        DateListener dateListener = new DateListener();
+        dateText.setOnClickListener(dateListener);
+        dateText.setOnLongClickListener(dateListener);
 
     }
 
@@ -87,5 +76,34 @@ public class ItemActivity extends AppCompatActivity {
 
 
 
+    }
+
+    class DateListener implements View.OnClickListener, View.OnLongClickListener {
+
+
+        @Override
+        public void onClick(View v) {
+
+            Calendar calendar = Calendar.getInstance();
+            int currYear = calendar.get(Calendar.YEAR);
+            int currMonth = calendar.get(Calendar.MONTH);
+            int currDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(ItemActivity.this,
+                    (datePicker, year, month, day) -> {
+                        dateText.setText(day + "\\" + month + "\\" + year );
+
+                    }, currYear, currMonth, currDay);
+            datePickerDialog.show();
+
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+
+            dateText.setText("date");
+            Log.i(MainActivity.TAG, "Long click");
+            return true;
+        }
     }
 }
