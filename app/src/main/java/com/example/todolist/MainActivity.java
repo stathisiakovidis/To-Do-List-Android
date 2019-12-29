385ba49ab67478ba880b7ab151eceb4174c09f46
grode <dimitris23bp@gmail.com>
2019-12-29 12:13:00 +0200

    Deleted unnecessary comments

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.todolist;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.example.todolist.database.AppDatabase;
-import com.example.todolist.database.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,63 +28,11 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(view ->
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show());
-
         fab.setOnClickListener(new FabListener());
 
         db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "todo-db").build();
 
-//        Save newTask = new Save();
-//        newTask.execute();
-
     }
-
-    /**
-     * Manually will add some data.
-     * This is a testing phase, it's not intended to be completely functional
-     */
-//    public class Save extends AsyncTask<Void, Void, Void> {
-//
-//        @Override
-//        protected Void doInBackground(Void... voids) {
-//            Task myTask = new Task();
-//            myTask.setTitle("Title again");
-//            myTask.setBody("Story of my life");
-//            myTask.setDay((byte) 16);
-//            myTask.setYear(2019);
-//            myTask.setMonth((byte) 11);
-//            db.userDao().insert(myTask);
-//
-//            Task secondTask = new Task(
-//                    "Another title",
-//                    "This is a small body",
-//                    13,
-//                    11,
-//                    2019);
-//            db.userDao().insert(secondTask);
-//
-//            Task thirdTask = new Task(
-//                    "Do some shit",
-//                    "I really have to do some shit, dude",
-//                    24,
-//                    11,
-//                    2019);
-//            db.userDao().insert(thirdTask);
-//
-//            ArrayList<Task> tasks = (ArrayList<Task>) db.userDao().getAll();
-//
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Void aVoid) {
-//            super.onPostExecute(aVoid);
-//            Log.i(TAG, "Insert is done");
-//
-//        }
-//    }
 
     //Creates option in menu (in example settings)
     @Override
